20e3e71e848ebde20c5131e86e41df1a45e8ebe6
pulpilisory <pulpilisory@gmailcom>
2024-05-11 16:53:26 +0900

    2024.05.11 by pdg : Search Todolist 작업
                - table search 기능 되는 view controller 생성
                - storyboard design 수정

---
+++ SQLiteTodoAdvanced/SQLiteTodoAdvanced/View/SearchTodoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchTodoViewController.swift
+//  SQLiteTodoAdvanced
+//
+//  Created by 박동근 on 5/11/24.
+//
+
+import UIKit
+
+class SearchTodoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
